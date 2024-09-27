a7907dddfb7c959abb0dfb49d203cb83f1c79a72
Tejaswa Kumar <viplavkumar408@gmail.com>
2024-09-27 13:12:40 +0530

    added new project

---
+++ side.js
@@ -0,0 +1,42 @@
+const sidebar =document.querySelector('#sidebar');
+
+const navBar =  document.querySelector("nav");
+const navLinks =  document.querySelector("nav ul");
+function openMenu()
+{
+    
+    sidebar.style.transform = 'translateX(-16rem)'
+}
+function closeMenu()
+{
+    sidebar.style.transform = 'translateX(16rem)';
+    
+}
+
+window.addEventListener('scroll', ()=>{
+    
+    if(scrollY > 50){
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
+    } else{ navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
+        
+    }
+} )
+
+// the dark mode 
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+
+  function toggletheme(){
+    document.documentElement.classList.toggle('dark');
+
+    if(document.documentElement.classList.contains('dark')){
+        localStorage.theme = 'dark';
+    }else{
+        localStorage.theme = 'light';
+    }
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme:{
+        extend:{
+    
+            colors:{
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a',
+                darkTheme: '#030737'
+            }
+        
+        }
+        
+    },
+    darkMode: 'selector'  
+}
